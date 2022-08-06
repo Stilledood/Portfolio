2e884fe6bd308433930bd2024e94e95d8d1dbf54
Florin <74854275+Stilledood@users.noreply.github.com>
2022-08-06 18:34:15 +0300

    Added Model for project

--- portfolio/myportfolio/models.py
+++ portfolio/myportfolio/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.shortcuts import reverse
 
-# Create your models here.
+
+class Project(models.Model):
+    '''Class to construct the model for all projects'''
+
+    title=models.CharField(max_length=255)
+    initial_description=models.TextField()
+    project_description=models.TextField()
+    summary_description=models.TextField()
+    image1=models.ImageField(upload_to='project_images',default=None)
+    image2=models.ImageField(upload_to='project_images',default=None)
+    image3=models.ImageField(upload_to='project_images',default=None)
+    github_link=models.URLField(max_length=255)
+    live_demo_link=models.URLField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('project_details',kwargs={'pk':self.pk})
+
+    
+
